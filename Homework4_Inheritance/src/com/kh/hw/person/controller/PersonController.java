6339f51b0fd5995e5c6ff36a3fdc58844d7f401d
U <U@desktop>
2026-01-06 01:56:14 +0900

    숙제

--- Homework4_Inheritance/src/com/kh/hw/person/controller/PersonController.java
+++ Homework4_Inheritance/src/com/kh/hw/person/controller/PersonController.java
@@ -6,26 +6,49 @@ import com.kh.hw.person.model.vo.Student;
 public class PersonController {
 	Student[] s = new Student[3];
 	Employee[] e = new Employee[10];
-	
+
+	// 각 객체배열에 저장된 객체의 수를 정수배열에 담아 반환하는 메소드
 	public int[] PersonCount() {
+		int sCount = 0;
+		int eCount = 0;
 		
-	}
-	
-	public void insertStudent(String name, int age, double height, double weight, 
-			int grade, String major) {
+		for(Student i : s) {
+			if(i != null) {
+				sCount++;
+			}
+		}
 		
+		for(Employee i : e) {
+			if(i != null) {
+				eCount++;
+			}
+		}
+		return new int[] {sCount, eCount};
 	}
-	
+
+	// 매개변수로 받아온 데이터를 학생 객체배열 중 빈 곳에 저장하는 메소드
+	public void insertStudent(String name, int age, double height, double weight, int grade, String major) {
+		
+		for (Student i : s) {
+			i = new Student(name, age, height, weight, grade, major);
+		}
+	}
+
+	// 학생 객체 배열의 주소를 반환하는 메소드
 	public Student[] printStudent() {
-		
+		return s;
 	}
-	
-	public void insertEmployee(String name, int age, double height, double weight, 
-			int salary, String dept) {
+
+	// 매개변수로 받아온 데이터를 사원 객체 배열 중 빈 곳에 저장되는 메소드
+	public void insertEmployee(String name, int age, double height, double weight, int salary, String dept) {
 		
+		for (Employee i : e) {
+			i = new Employee(name, age, height, weight, salary, dept);
+		}
 	}
-	
+
+	// 사원 객체 배열의 주소를 반환하는 메소드
 	public Employee[] printEmplyee() {
-		
+		return e;
 	}
 }
--- Homework4_Inheritance/src/com/kh/hw/person/view/PersonMenu.java
+++ Homework4_Inheritance/src/com/kh/hw/person/view/PersonMenu.java
@@ -14,11 +14,18 @@ public class PersonMenu {
 
 		while (true) {
 			int[] currentCount = pc.PersonCount();
+
+			System.out.println("학생은 최대 10명까지 저장할 수 있습니다.");
+			System.out.println("현재 저장된 학생은 " + currentCount[0] + "명입니다.");
+			System.out.println("사원은 최대 3명까지 저장할 수 있습니다.");
+			System.out.println("현재 저장된 학생은 " + currentCount[1] + "명입니다.");
+			System.out.println();
 			System.out.println("1. 학생 메뉴");
 			System.out.println("2. 사원 메뉴");
 			System.out.println("9. 끝내기");
 			System.out.print("메뉴 번호 : ");
 			int menu = sc.nextInt();
+			System.out.println();
 
 			switch (menu) {
 			case 1:
@@ -38,21 +45,26 @@ public class PersonMenu {
 	public void studentMenu() {
 		while (true) {
 			int[] currentCount = pc.PersonCount();
+
 			System.out.println("1. 학생 추가");
 			System.out.println("2. 학생 보기");
 			System.out.println("9. 메인으로");
 			System.out.print("메뉴 번호 : ");
 			int menu = sc.nextInt();
+			System.out.println();
+			
+			if (currentCount[0] == 3) {
+				System.out.println("학생을 담을 수 있는 공간이 꽉 찼기 때문에 학생 추가를 종료하고 학생메뉴로 돌아갑니다.");
+				return;
+			}
 
 			switch (menu) {
 			case 1:
 				if (currentCount[0] == 3) {
-					System.out.println("추가 메뉴는 더이상 활성화되지 않습니다.");
-					return;
+					System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
 				} else {
 					insertStudent();
 				}
-				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
 				break;
 			case 2:
 				printStudent();
@@ -69,16 +81,19 @@ public class PersonMenu {
 	public void employeeMenu() {
 		while (true) {
 			int[] currentCount = pc.PersonCount();
-			
+
 			System.out.println("1. 사원 추가");
 			System.out.println("2. 사원 보기");
 			System.out.println("9. 메인으로");
-			if (currentCount[1] == 10) {
-				System.out.println("추가 메뉴는 더이상 활성화되지 않습니다.");
-			}
 			System.out.print("메뉴 번호 : ");
 			int menu = sc.nextInt();
+			System.out.println();
 
+			if (currentCount[1] == 10) {
+				System.out.println("학생을 담을 수 있는 공간이 꽉 찼기 때문에 학생 추가를 종료하고 학생메뉴로 돌아갑니다.");
+				return;
+			}
+			
 			switch (menu) {
 			case 1:
 				if (currentCount[1] == 10) {
@@ -100,33 +115,39 @@ public class PersonMenu {
 	}
 
 	public void insertStudent() {
-		int[] currentCount = pc.PersonCount();
-		System.out.print("학생 이름 : ");
-		String name = sc.next();
-		System.out.print("학생 나이 : ");
-		int age = sc.nextInt();
-		System.out.print("학생 키 : ");
-		double height = sc.nextDouble();
-		System.out.print("학생 몸무게 : ");
-		double weight = sc.nextDouble();
-		System.out.print("학생 학년 : ");
-		int grade = sc.nextInt();
-		System.out.print("학생 전공 : ");
-		String major = sc.next();
-
-		pc.insertStudent(name, age, height, weight, grade, major);
 		while (true) {
-			if (currentCount[2] != 3) {
-				System.out.println("이어하시려면 아무 키나 누르세요 : ");
+			int[] currentCount = pc.PersonCount();
+
+			if (currentCount[0] == 3) {
+				System.out.println("학생을 담을 수 있는 공간이 꽉 찼기 때문에 학생 추가를 종료하고 학생메뉴로 돌아갑니다.");
+				break;
+			}
+
+			System.out.print("학생 이름 : ");
+			String name = sc.next();
+			System.out.print("학생 나이 : ");
+			int age = sc.nextInt();
+			System.out.print("학생 키 : ");
+			double height = sc.nextDouble();
+			System.out.print("학생 몸무게 : ");
+			double weight = sc.nextDouble();
+			System.out.print("학생 학년 : ");
+			int grade = sc.nextInt();
+			System.out.print("학생 전공 : ");
+			String major = sc.next();
+
+			pc.insertStudent(name, age, height, weight, grade, major);
+			
+			// 추가 후 다시 개수 세어서 판단
+			currentCount = pc.PersonCount();
+
+			if (currentCount[0] < 3) {
+				System.out.print("그만 하시려면N(또는 n), 이어하시려면 아무 키나 누르세요 : ");
 				char ch = sc.next().toUpperCase().charAt(0);
 
 				if (ch == 'N') {
-					return;
+					break;
 				}
-			} else {
-				System.out.println("학생 메뉴로 돌아갑니다.");
-				//studentMenu();
-				break;
 			}
 		}
 	}
@@ -137,35 +158,39 @@ public class PersonMenu {
 	}
 
 	public void insertEmployee() {
-		int[] currentCount = pc.PersonCount();
-		System.out.print("사원 이름 : ");
-		String name = sc.next();
-		System.out.print("사원 나이 : ");
-		int age = sc.nextInt();
-		System.out.print("사원 키 : ");
-		double height = sc.nextDouble();
-		System.out.print("사원 몸무게 : ");
-		double weight = sc.nextDouble();
-		System.out.print("사원 급여 : ");
-		int salary = sc.nextInt();
-		System.out.print("사원 부서 : ");
-		String dept = sc.next();
-
-		pc.insertEmployee(name, age, height, weight, salary, dept);
-		
 		while (true) {
+			int[] currentCount = pc.PersonCount();
+
+			if (currentCount[1] == 10) {
+				System.out.println("학생을 담을 수 있는 공간이 꽉 찼기 때문에 학생 추가를 종료하고 학생메뉴로 돌아갑니다.");
+				break;
+			}
+
+			System.out.print("사원 이름 : ");
+			String name = sc.next();
+			System.out.print("사원 나이 : ");
+			int age = sc.nextInt();
+			System.out.print("사원 키 : ");
+			double height = sc.nextDouble();
+			System.out.print("사원 몸무게 : ");
+			double weight = sc.nextDouble();
+			System.out.print("사원 급여 : ");
+			int salary = sc.nextInt();
+			System.out.print("사원 부서 : ");
+			String dept = sc.next();
+
+			pc.insertEmployee(name, age, height, weight, salary, dept);
+
+			currentCount = pc.PersonCount();
+			
 			if (currentCount[1] != 10) {
-				System.out.println("이어하시려면 아무 키나 누르세요 : ");
+				System.out.print("그만 하시려면N(또는 n), 이어하시려면 아무 키나 누르세요 : ");
 				char ch = sc.next().toUpperCase().charAt(0);
 
 				if (ch == 'N') {
-					return;
+					break;
 				}
-			} else {
-				System.out.println("사원 메뉴로 돌아갑니다.");
-				//employeeMenu();
-				break;
-			}
+			} 
 		}
 	}
 
